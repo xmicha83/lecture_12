4b056a42c5b79dbd0e7e3cb0c0960e09390f15ae
Eva Michalkova <xmicha83@vutbr.cz>
2021-04-27 18:19:31 +0200

    18:19

--- sort_items.py
+++ sort_items.py
@@ -10,6 +10,15 @@ def read_row(file_name):
     :param file_name: (str), name of CSV file
     :return: (list, int),
     """
+    with open(file_name, "r", encoding="utf-8") as file:
+        soubor = csv.reader(file, delimiter="\t")
+        values = []
+        for line in soubor:
+            row = [int(number) for number in line]
+
+    return row
+
+
 
 
 def read_rows(file_name, row_number):
@@ -19,14 +28,35 @@ def read_rows(file_name, row_number):
     :param row_number: (int), number of selected row
     :return: (list, int),
     """
+    with open(file_name, "r", encoding="utf-8") as file:
+        soubor = csv.reader(file)
+        # rows = []
+        for idx, line in enumerate(soubor):
+            row = [int(number) for number in line]
+    #         rows.append(row)
+    #
+    # return rows[row_number-1]
 
 
-def selection_sort(number_array):
+def selection_sort(number_array, direction="ascending"):
     """
         Sorts and returns selected numeric data with Selection Sort.
         :param number_array: (list,int), list with numeric array
         :return: (list, int), sorted numeric array
     """
+
+    for i in range(len(number_array)):
+        minimum = i
+        for j in range(i+1, len(number_array)):
+            if direction == "ascending":
+                if number_array[j] < number_array[minimum]:
+                    minimum = j
+            if direction == "descending":
+                if number_array[j] > number_array[minimum]:
+                    minimum = j
+        number_array[i], number_array[minimum] = number_array[minimum], number_array[i]
+
+    return number_array
 
 
 def bubble_sort(number_array):
@@ -35,12 +65,26 @@ def bubble_sort(number_array):
        :param number_array: (list,int), list with numeric array
        :return: (list, int), sorted numeric array
     """
+    for i in range(len(number_array)-1):
+        for j in range(len(number_array)-i-1):
+            if number_array[j] > number_array[j+1]:
+                number_array[j], number_array[j+1] = number_array[j+1], number_array[j]
 
 
 def main():
 
-    # Ukol: Selection Sort
+    data = read_row('numbers_one.csv')
+    print(data)
 
+    data2 = read_rows('numbers_two.csv', 2)
+    print(data2)
+
+    # Ukol: Selection Sort
+    sel_sort = selection_sort(data, 'ascending')
+    print(sel_sort)
+
+    bubble = bubble_sort(data2)
+    print(bubble)
 
     # Ukol: Selection Sort - se smerem razeni
     
